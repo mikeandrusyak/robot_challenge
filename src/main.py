4b63b4d70554708ed26502eeb3fea830d3858dc7
mikeandrusyak <andrusyakmd@gmail.com>
2025-03-25 14:04:49 +0100

    sync main line_folowing jup notebook

--- src/main.py
+++ src/main.py
@@ -61,17 +61,32 @@ def log_event(action):
     log[action] = log.setdefault(action, [])
     log[action].append(timestamp)
 
-def qr_code_command(message, number_of_objects):
+def circle(turn, speed, number_of_objects, threshold):
+    for j in range(number_of_objects):
+        for i in range(4):
+            zumi.reset_gyro()
+            if turn == 'left':
+                zumi.turn_left(90)
+                log_event('move_left')
+            elif turn == 'right':
+                zumi.turn_right(90)
+                log_event('move_right')
+            desired_angle = zumi.read_z_angle()
+            desired_angle = turning_correction(desired_angle, 90)
+            front_right, bottom_right, back_right, bottom_left, back_left, front_left = zumi.get_all_IR_data()
+            zumi.reset_gyro()
+            while bottom_left > threshold or bottom_right > threshold:
+                front_right, bottom_right, back_right, bottom_left, back_left, front_left = zumi.get_all_IR_data()
+                desired_angle = line_correction(bottom_left, bottom_right, desired_angle, threshold)
+                zumi.go_straight(speed, desired_angle)
+            
+            
+
+def qr_code_command(message, speed, number_of_objects, threshold):
     if message == "Left Circle":
-        for j in range(number_of_objects):
-            for i in range(4):
-                zumi.turn_right(90)
-                zumi.line_follower(3)
+        circle('left', speed, number_of_objects, threshold)
     elif message == "Right Circle":
-        for j in range(number_of_objects):
-            for i in range(4):
-                zumi.turn_right(90)
-                zumi.line_follower(3)
+        circle('right', speed, number_of_objects, threshold)
     elif message == "Turn Left":
         zumi.turn_left(90)
     elif message == "Turn Right":
@@ -87,13 +102,13 @@ def qr_code_command(message, number_of_objects):
     else:
         print("Invalid command")
 
-def read_qr_code(number_of_objects):
+def read_qr_code(speed, number_of_objects, threshold):
     camera.start_camera()
     frame = camera.capture()
     camera.close()
     qr_code = vision.find_QR_code(frame)
     message = vision.get_QR_message(qr_code)
-    qr_code_command(message, number_of_objects)
+    qr_code_command(message, speed, number_of_objects, threshold)
     return message
 
 def line_correction(bottom_left, bottom_right, desired_angle, threshold):
@@ -136,7 +151,7 @@ def finish_with_180_turn():
 def save_dict_to_csv(data_dict):
     # Generate file name with current time
     current_time = datetime.now().strftime('%Y%m%d_%H%M%S')
-    file_name = f"output_{current_time}.csv"
+    file_name = "output_" + current_time + ".csv"
 
     # Create empty list to store rows
     rows = []
@@ -163,6 +178,7 @@ desired_angle = zumi.read_z_angle()
 
 log_event('start')
 log_event('end_line')
+
 try:
     while True:
         # Set the threshold for the IR sensors and the speed
@@ -170,10 +186,11 @@ try:
         speed = 5
 
         if object_detected():
+            zumi.stop()
             log_event('object_detected')
             number_of_objects += 1
             zumi.play_note(13, 500) # 13 is note type (1 - 60), 500 is duration in ms
-            screen.draw_text_center(f"Objects: {number_of_objects}") # Display object count on screen
+            screen.draw_text_center("Objects: " + str(number_of_objects)) # Display object count on screen
             # Wait until the object is removed
             print("Waiting for object to be removed...")
             while object_detected():
@@ -182,8 +199,8 @@ try:
             log_event('object_removed')
             print("Object removed. Resuming movement.")
             log_event('qr_code_read')
-            message = read_qr_code(number_of_objects)
-            log_event('qr_code_command: ' + message + " done")
+            message = read_qr_code(speed, number_of_objects, threshold)
+            log_event('qr_code_command: ' + str(message) + " done")
             
 
         # Read all IR sensor values
